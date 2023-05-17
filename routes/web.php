ac08c57ae4ac78d31140704b08bb7cbf2627f828
Widi Arrohman <widiarrohman1234@gmail.com>
2023-05-17 22:06:57 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
+
+Route::get('todo/index', function () {
+    return view('todo.index');
 });
