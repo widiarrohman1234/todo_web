341a18f0cdd6498b0078e3d94deaf83b47d04e73
Widi Arrohman <widiarrohman1234@gmail.com>
2023-05-19 10:32:25 +0700

    migtration

--- database/migrations/2023_05_18_094226_create_table_todos.php
+++ database/migrations/2023_05_18_094226_create_table_todos.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->String('todo');
+            $table->boolean('status_finish')->default(false);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('todos');
     }
 };
