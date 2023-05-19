3450a2cf69788e229c4e3d375da6da1515af5307
Widi Arrohman <widiarrohman1234@gmail.com>
2023-05-19 10:33:08 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,18 @@ Route::get('register', function () {
     return view('register');
 });
 
-Route::get('todo/index', function () {
-    return view('todo.index');
-});
+// Route::get('todo/index', function () {
+//     return view('todo.index');
+// });
+
+Route::post('register', [AuthController::class, 'store']);
+Route::get('login', function () {
+    return view('login');
+})->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::get('logout', [AuthController::class, 'logout']);
+
+Route::get('todos/index', [TodoController::class, 'index'])->name('todos')->middleware('auth');;
+Route::post('todos-status/{id}', [TodoController::class, 'todoStatus']);
+Route::post('todos', [TodoController::class, 'store']);
+Route::delete('todos/{id}', [TodoController::class, 'destroy']);
