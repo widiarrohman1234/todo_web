2df5b11b5659a6b04731290fa1398486a20e85d1
Widi Arrohman <widiarrohman1234@gmail.com>
2023-05-20 13:59:54 +0700

    asset to url

--- system/resources/views/login.blade.php
+++ system/resources/views/login.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('publlic/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/font-awesome-4.7.0/css/font-awesome.min.css')}}">
     <title>Hello, world!</title>
     <style>
         .divider:after,
