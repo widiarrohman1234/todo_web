0c8fc073d3b754146ffd952b499fefc76e969b86
ASUS <talitazmiza@gmail.com>
2023-06-03 09:49:21 +0700

    add edit, update, create, store method

--- system/app/Http/Controllers/TodoController.php
+++ system/app/Http/Controllers/TodoController.php
@@ -16,11 +16,17 @@ class TodoController extends Controller
         $data['todo_list'] = Todo::where('id_user', $id_user)->get();
         $data['user'] = User::where('id', $id_user)->first();
         $data['list_user'] = DB::table('users')->select('*')->get();
+        $data['todo_lists'] = DB::table('todos')->select('*')->get();
         return view('todo.index', $data);
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        Todo::create([
+            'todo'=>$request->get('todo')
+        ]);
+               return redirect()->route('todos.index')->with('success', 'Inserted');
+
     }
 
     public function store(Request $request)
@@ -44,16 +50,32 @@ class TodoController extends Controller
     public function edit($id)
     {
         //
+        $todo=Todo::where('id',$id)->first();
+        return view('edit-todo',compact('todo'));
     }
 
     public function update(Request $request, $id)
     {
         //
+        $validator = Validator::make($request->all(), [
+            'todos' => 'required',
+        ]);
+
+        if ($validator->fails())
+        {
+            return redirect()->route('todos.edit',['todo'=>$id])->withErrors($validator);
+        }
+        $todo=Todo::where('id',$id)->first();
+        $todo->title=$request->get('todos');
+        $todo->is_completed=$request->get('status_finish');
+        $todo->save();
+
+        return redirect()->route('todos')->with('success', 'Updated Todo');
     }
 
     public function destroy($id)
     {
         $delete = DB::table('todos')->where('todos.id', $id)->delete();
         return redirect('todos/index');
-    }
+    }  
 }
