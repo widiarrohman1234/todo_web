a13b48bb687f5f951d9ca3f306bb9b17b03fd0af
ASUS <talitazmiza@gmail.com>
2023-06-03 09:47:12 +0700

    add list of todos

--- system/resources/views/todo/index.blade.php
+++ system/resources/views/todo/index.blade.php
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarExample01">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" aria-current="page" href="#">Users</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{url('logout')}}">Logout</a>
@@ -46,6 +46,66 @@
             @endforeach
         </tbody>
     </table>
+
+    <div class="text-center mt-5">
+    <h2>Add Todo</h2>
+
+    <form class="row g-3 justify-content-center" method="POST" action="{{route('todos.store')}}">
+        @csrf
+        <div class="col-6">
+            <input type="text" class="form-control" name="title" placeholder="Title">
+        </div>
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Submit</button>
+        </div>
+    </form>
+
+    <div class="text-center">
+    <h2>All Todos</h2>
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Todo</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Created at</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                @php $counter=1 @endphp
+
+                @foreach($todo_lists as $t)
+                    <tr>
+                        <th>{{$counter}}</th>
+                        <td>{{$t->todos}}</td>
+                        <td>
+                            @if($t->status_finish)
+                                <div class="badge bg-success">Completed</div>
+                            @else
+                                <div class="badge bg-warning">Not Completed</div>
+                            @endif
+                        </td>
+                        <td>{{$t->created_at}}</td>
+                        <td>
+                            <a class="btn btn-danger">Edit</a>
+                            <a class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+
+                    @php $counter++; @endphp
+
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
 </div>
 <!-- /Jumbotron -->
 
