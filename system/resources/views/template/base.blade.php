ce42413c37f6fb1e051be6a1b53b6d2858533fd7
Widi Arrohman <widiarrohman1234@gmail.com>
2023-06-04 01:00:30 +0700

    okeh, jadikan aku yang terakhir

--- system/resources/views/template/base.blade.php
+++ system/resources/views/template/base.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('font-awesome-4.7.0/css/font-awesome.min.css')}}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <title>Document</title>
+    <title>Todo List</title>
 </head>
 
 <body>
